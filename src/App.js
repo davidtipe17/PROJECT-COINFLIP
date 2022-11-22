885b5a9f805b98bb465267f9695b3c3b06e396bf
david marco antonio tipe palomino <davidmarcotipe@gmail.com>
2022-11-21 21:43:42 -0500

    chance final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useRef } from "react";
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 function App() {
+
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className>
+      <div className="head">
+        <div className="logo">
+          <a href="#">COINFLIP</a>
+        </div>
+        <div className='div-container-nav'> 
+        <nav className="navbar" ref={navRef}>
+          <button className='nav-btn' onClick={showNavbar}>
+            <IoIosArrowRoundForward/>
+          </button>
+          <a href="#">Buy / Sell</a>
+          <a href="#">Grow</a>
+          <a href="#">Markets</a>
+          <a href="#">Business</a>
+          <a href="#">Support</a>
+        </nav>
+        <button className='nav-btn nav-close-btn' onClick={showNavbar} >
+          <FaBars />
+        </button>
+        </div>
+        <div className="navlink" >
+          <button className="btn-signin">Sign In</button>
+          <button className="btn-signup">Sign Up</button>
+        </div>
+      </div>
+      <div className="content">
+        <div className='content-center'>
+          <h1 className="title">We make crypto clear and simple</h1>
+          <h3 className="description"> Buy, sell, and grow your crypto with CoinFlip, the platform dedicated to every trader at every level.</h3>
+        </div>
+        <button className="btn-getstarted">
+          Get Started
+        </button>
+      </div>
     </div>
   );
 }
